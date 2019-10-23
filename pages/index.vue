3b6e47f1779ebdb555ab3c271c30da60758a6c8a
Omar Bin Salamah <obinsalamah@gmail.com>
2019-10-23 17:56:23 -0500

    :construction:

--- pages/index.vue
+++ pages/index.vue
@@ -1,24 +1,61 @@
 <template>
   <v-flex align-center justify-center>
-    <v-card flat>
+    <v-card id="info-card" flat>
       <v-divider />
       <h1>Omar Aziz</h1>
       <h2>software developer</h2>
       <h2>former infant</h2>
       <h3>click on the top left to explore this site</h3>
+      <br>
       <v-divider />
+      <v-row justify="center">
+        <v-dialog
+          v-model="dialog"
+          transition="dialog-bottom-transition"
+          width="400"
+          :hide-overlay="false"
+        >
+          <template v-slot:activator="{ on }">
+            <span style="font-size: 17.5px; font-weight: 700; color: #dfdfdf ">&nbsp;&nbsp;&nbsp;connect</span>
+            <v-btn icon v-on="on">
+              <v-icon style="">
+                mail
+              </v-icon>
+            </v-btn>
+          </template>
+          <v-card>
+            <xyzMail
+              :id=" xyzMailProps.id"
+              :styling="xyzMailProps.class"
+            />
+          </v-card>
+        </v-dialog>
+      </v-row>
     </v-card>
   </v-flex>
 </template>
 
 <script>
+import xyzMail from '~/components/xyzMail/xyzMail.vue'
 export default {
+  components: {
+    xyzMail
+  },
+  data: () => {
+    return {
+      dialog: false,
+      xyzMailProps: {
+        id: 0,
+        class: ''
+      }
+    }
+  }
 
 }
 </script>
 
 <style lang="css" scoped>
-.v-card {
+#info-card {
   background: transparent;
   margin: 80px 30px 0px 30px;
 }
@@ -30,5 +67,9 @@ h1 {
   font-weight: bolder;
   letter-spacing: 0.5rem;
   color: #c3b6a3
+}
+
+.v-item-group > * {
+  flex: 0
 }
 </style>
